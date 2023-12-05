358374a1932c937879ca32b375f98d3d3d03ba80
carlosTertuliano <carlostertulliano07@gmail.com>
2023-12-05 19:29:39 -0300

    correção parcial do contador

--- memory_game_implementation/contador.vhd
+++ memory_game_implementation/contador.vhd
@@ -6,28 +6,42 @@ ENTITY contador IS
     GENERIC (W : NATURAL := 3);
     PORT (
         d : IN STD_LOGIC_VECTOR(W-1 DOWNTO 0); -- data input
-        clk : IN BIT; -- clock
-        clrn : IN BIT; -- clear
+		  CLK : IN STD_LOGIC;
         ena : IN BIT; -- enable
-        load : IN BIT; -- load
         q : OUT STD_LOGIC_VECTOR(W-1 DOWNTO 0) -- data output
     );
 END contador;
 
 ARCHITECTURE arch_1 OF contador IS
 
-signal saida : STD_LOGIC_VECTOR(3 DOWNTO 0) := "000";
+component registrador3bits is
+        generic(W : NATURAL := 3);
+        port (
+            d : in STD_LOGIC_VECTOR(W-1 DOWNTO 0);
+            clk : in STD_LOGIC;
+            clrn : in STD_LOGIC;
+            ena : in STD_LOGIC;
+            q : out STD_LOGIC_VECTOR(W-1 DOWNTO 0)
+        );
+    end component;
+
+signal saida : STD_LOGIC_VECTOR(3 DOWNTO 0);
 
 BEGIN
-    PROCESS (clk, clrn)
+		cont : registrador3bits
+		port map(
+			d => d,
+         clk => CLK,
+			clrn => "1",
+			ena => "1",
+         q => saida
+		);
+
+    PROCESS (ena)
     BEGIN
-        IF (clrn = '0') THEN
-            saida <= (OTHERS => '0');
-        ELSIF (clk'EVENT AND clk = '1') THEN
-            IF (ena = '1') THEN
-					saida <= saida + 1;
-					q <= saida;
-            END IF;
-        END IF;
+		IF (ena = '1') THEN
+			saida <= saida + 1;
+			q <= saida;
+		END IF;
     END PROCESS;
 END arch_1;
